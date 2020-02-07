1cbd152cfa64e73a2e8fb49eab8b43456815e016
RainbowMango <renhongcai@huawei.com>
2020-02-07 18:52:50 +0800

    Add debug log for indicate when the interface is called.

--- pkg/cloudprovider/huaweicloud/instances.go
+++ pkg/cloudprovider/huaweicloud/instances.go
@@ -42,6 +42,7 @@ var _ cloudprovider.Instances = &Instances{}
 // returns the address of the calling instance. We should do a rename to
 // make this clearer.
 func (i *Instances) NodeAddresses(ctx context.Context, name types.NodeName) ([]v1.NodeAddress, error) {
+	klog.Infof("NodeAddresses is called. input name: %s", name)
 	return nil, nil
 }
 
@@ -51,6 +52,7 @@ func (i *Instances) NodeAddresses(ctx context.Context, name types.NodeName) ([]v
 // from the node whose nodeaddresses are being queried. i.e. local metadata
 // services cannot be used in this method to obtain nodeaddresses
 func (i *Instances) NodeAddressesByProviderID(ctx context.Context, providerID string) ([]v1.NodeAddress, error) {
+	klog.Infof("NodeAddressesByProviderID is called. input provider ID: %s", providerID)
 	return []v1.NodeAddress{}, cloudprovider.NotImplemented
 }
 
@@ -58,22 +60,26 @@ func (i *Instances) NodeAddressesByProviderID(ctx context.Context, providerID st
 // Note that if the instance does not exist, we must return ("", cloudprovider.InstanceNotFound)
 // cloudprovider.InstanceNotFound should NOT be returned for instances that exist but are stopped/sleeping
 func (i *Instances) InstanceID(ctx context.Context, nodeName types.NodeName) (string, error) {
+	klog.Infof("InstanceID is called. input nodeName: %s", nodeName)
 	return "", nil
 }
 
 // InstanceType returns the type of the specified instance.
 func (i *Instances) InstanceType(ctx context.Context, name types.NodeName) (string, error) {
+	klog.Infof("InstanceType is called. input name: %s", name)
 	return "", nil
 }
 
 // InstanceTypeByProviderID returns the type of the specified instance.
 func (i *Instances) InstanceTypeByProviderID(ctx context.Context, providerID string) (string, error) {
+	klog.Infof("InstanceTypeByProviderID is called. input provider ID: %s", providerID)
 	return "", cloudprovider.NotImplemented
 }
 
 // AddSSHKeyToAllInstances adds an SSH public key as a legal identity for all instances
 // expected format for the key is standard ssh-keygen format: <protocol> <blob>
 func (i *Instances) AddSSHKeyToAllInstances(ctx context.Context, user string, keyData []byte) error {
+	klog.Infof("AddSSHKeyToAllInstances is called. input user: %s, keyData: %v", user, keyData)
 	return cloudprovider.NotImplemented
 }
 
@@ -88,11 +94,13 @@ func (i *Instances) CurrentNodeName(ctx context.Context, hostname string) (types
 // If false is returned with no error, the instance will be immediately deleted by the cloud controller manager.
 // This method should still return true for instances that exist but are stopped/sleeping.
 func (i *Instances) InstanceExistsByProviderID(ctx context.Context, providerID string) (bool, error) {
+	klog.Infof("InstanceExistsByProviderID is called. input provider ID: %s", providerID)
 	return false, cloudprovider.NotImplemented
 }
 
 // InstanceShutdownByProviderID returns true if the instance is shutdown in cloudprovider
 func (i *Instances) InstanceShutdownByProviderID(ctx context.Context, providerID string) (bool, error) {
+	klog.Infof("InstanceShutdownByProviderID is called. input provider ID: %s", providerID)
 	return false, cloudprovider.NotImplemented
 }
 
